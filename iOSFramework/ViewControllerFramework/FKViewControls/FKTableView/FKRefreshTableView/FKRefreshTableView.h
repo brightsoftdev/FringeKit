57956e72d3c644ed79a3fcfa76ecdd0bcaf96f07
zdnet <zdnet214@gmail.com>
2011-11-07 17:30:13 +0800

    init FKRefreshTableView

---
+++ iOSFramework/ViewControllerFramework/FKViewControls/FKTableView/FKRefreshTableView/FKRefreshTableView.h
@@ -0,0 +1,13 @@
+//
+//  FKRefreshTableView.h
+//  iOSFramework
+//
+//  Created by  on 11/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FKTableView.h"
+
+@interface FKRefreshTableView : FKTableView
+
+@end
