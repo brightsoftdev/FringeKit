f82cddb59cad0ac9fa48a4dad5845c583f64490b
zdnet <zdnet214@gmail.com>
2011-11-17 11:50:30 +0800

    init FKObject

---
+++ iOSFramework/ServiceFramework/FKObject.m
@@ -0,0 +1,13 @@
+//
+//  FKObject.m
+//  iOSFramework
+//
+//  Created by  on 11/17/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FKObject.h"
+
+@implementation FKObject
+
+@end
